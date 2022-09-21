8f54e5837decee3889d9a61516e656ce573e0b23
JoanMarcPC <101581954+JoanMarcPC@users.noreply.github.com>
2022-09-21 19:28:57 +0200

    v1

---
+++ S04T01N01PedroCasullaJoanMarc/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/S04T01N01PedroCasullaJoanMarc/S04T01N01PedroCasullaJoanMarcApplication.java
@@ -0,0 +1,14 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.S04T01N01PedroCasullaJoanMarc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01PedroCasullaJoanMarcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01PedroCasullaJoanMarcApplication.class, args);
+
+	}
+
+}
